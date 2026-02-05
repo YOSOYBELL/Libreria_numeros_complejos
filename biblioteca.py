f35592d272869c10efe47815fa2d436553a1ca65
YOSOYBELL <diego.rramirez@mail.escuelaing.edu.co>
2026-02-05 18:54:07 -0500

    cambio biblioteca y pruebas

--- biblioteca.py
+++ biblioteca.py
@@ -1,104 +1,41 @@
 import math
 
 def sumaCplx(c1, c2):
-    # Suma complejos representados como una tupla
-    numReal = c1[0] + c2[0]
-    numImg = c1[1] + c2[1]
-    res = numReal, numImg
-    return res
-
-def prodCplx(c1, c2):
-    # Multiplica complejos representados como una tupla
-    a1a2 = c1[0] * c2[0]
-    b1b2 = c1[1] * c2[1]
-    a1b2 = c1[0] * c2[1]
-    a2b1 = c2[0] * c1[1]
-    multiplication = (a1a2 - b1b2, a1b2 + a2b1)
-    return multiplication
+    return (c1[0] + c2[0], c1[1] + c2[1])
 
 def restaCplx(c1, c2):
-    # Resta complejos representados como una tupla
-    numReal = c1[0] - c2[0]
-    numImg = c1[1] - c2[1]
-    res = numReal, numImg
-    return res
+    return (c1[0] - c2[0], c1[1] - c2[1])
+
+def prodCplx(c1, c2):
+    a1, b1 = c1
+    a2, b2 = c2
+    return (a1*a2 - b1*b2, a1*b2 + a2*b1)
 
 def divisionCplx(c1, c2):
-    # Divide complejos representados como una tupla
-    a1a2 = c1[0] * c2[0]
-    b1b2 = c1[1] * c2[1]
-    a1b2 = c1[0] * c2[1]
-    a2b1 = c2[0] * c1[1]
-    a2 = (c2[0]) ** 2
-    b2 = (c2[1]) ** 2
+    a1, b1 = c1
+    a2, b2 = c2
+    denominador = a2**2 + b2**2
+    real = (a1*a2 + b1*b2) / denominador
+    imag = (a2*b1 - a1*b2) / denominador
+    return (round(real, 2), round(imag, 2))
 
-    Parte1 = a2 + b2
-    Parte2 = a1a2 + b1b2
-    Parte3 = a2b1 - a1b2
-    return (round(Parte2 / Parte1, 2), round(Parte3 / Parte1, 2))
+def moduloClpx(c):
+    return round(math.sqrt(c[0]**2 + c[1]**2), 2)
 
-def moduloClpx(c1):
-    # Saca el modulo de un complejo representado como una tupla
-    module = math.sqrt(((c1[0]) ** 2 + (c1[1]) ** 2))
-    return round(module, 2)
+def conjugadoClpx(c):
+    return (c[0], -c[1])
 
-def conjugadoClpx(c1):
-    # Conjugado de un número representado como una tupla
-    numReal = c1[0]
-    numImg = c1[1]
+def conversionPolarCartesianoClpx(c):
+    r, theta = c
+    real = r * math.cos(theta)
+    imag = r * math.sin(theta)
+    return (round(real, 2), round(imag, 2))
 
-    if numImg < 0:
-        numImg = abs(numImg)
-    else:
-        numImg = numImg
-    return (numReal, numImg)
+def conversionCartesianoPolarClpx(c):
+    a, b = c
+    r = math.sqrt(a**2 + b**2)
+    theta = math.atan2(b, a)
+    return (round(r, 2), round(theta, 2))
 
-# Conversión entre representaciones polar y cartesiano
-def conversionPolarCartesianoClpx(c1):
-    a = c1[0] * math.cos(c1[1])
-    b = c1[0] * math.sin(c1[1])
-    return (round(a), round(b, 2))
-
-def conversionCartesianoPolarClpx(c1):
-    a = c1[0]
-    b = c1[1]
-    rho = math.sqrt(a ** 2 + b ** 2)
-    theta = math.atan(b / a)
-    return (round(rho, 2), round(theta, 2))
-
-def faseClpx(c1):
-    # Retornar la fase de un número complejo.
-    a = c1[0]
-    b = c1[1]
-    theta = math.atan(b / a)
-    return round(theta, 2)
-
-def prettyprinting(c):
-    # Para cartesianos
-    if c[1] < 0:
-        print(str(c[0]) + str(c[1]) + "i")
-    elif c[1] == 0:
-        print(str(c[0]))
-    else:
-        print(str(c[0]) + "+" + str(c[1]) + "i")
-
-def polprettyprinting(c):
-    # Para polares
-    print(str(c[0]) + " e^(i " + str(c[1]) + ")")
-
-c1 = (2, -1)
-c2 = (-1, 3)
-print()
-print('------------  COMIENZO PRUEBAS ------------')
-prettyprinting(sumaCplx(c1, c2))
-prettyprinting(restaCplx(c1, c2))
-prettyprinting(prodCplx(c1, c2))
-prettyprinting(divisionCplx(c1, c2))
-print(moduloClpx(c1))
-prettyprinting(conjugadoClpx(c1))
-prettyprinting(conversionPolarCartesianoClpx(c1))
-polprettyprinting(conversionCartesianoPolarClpx(c1))
-print(faseClpx(c1))
-print()
-print('------------  FINAL PRUEBAS ------------')
-print()
+def faseClpx(c):
+    return round(math.atan2(c[1], c[0]), 2)
--- test_complejos.py
+++ test_complejos.py
@@ -3,48 +3,59 @@ import biblioteca
 
 class TestComplejos(unittest.TestCase):
 
-    def test_suma(self):
-        self.assertEqual(
-            biblioteca.sumaCplx((2, -1), (-1, 3)),
-            (1, 2)
-        )
+    def test_suma_1(self):
+        self.assertEqual(biblioteca.sumaCplx((2, -1), (-1, 3)), (1, 2))
 
-    def test_resta(self):
-        self.assertEqual(
-            biblioteca.restaCplx((2, -1), (-1, 3)),
-            (3, -4)
-        )
+    def test_suma_2(self):
+        self.assertEqual(biblioteca.sumaCplx((0, 0), (3, 4)), (3, 4))
 
-    def test_producto(self):
-        self.assertEqual(
-            biblioteca.prodCplx((2, -1), (-1, 3)),
-            (1, 7)
-        )
+    def test_resta_1(self):
+        self.assertEqual(biblioteca.restaCplx((2, -1), (-1, 3)), (3, -4))
 
-    def test_division(self):
-        self.assertEqual(
-            biblioteca.divisionCplx((2, -1), (-1, 3)),
-            (-0.5, -0.5)
-        )
+    def test_resta_2(self):
+        self.assertEqual(biblioteca.restaCplx((5, 5), (2, 3)), (3, 2))
 
-    def test_modulo(self):
-        self.assertEqual(
-            biblioteca.moduloClpx((3, 4)),
-            5.0
-        )
+    def test_producto_1(self):
+        self.assertEqual(biblioteca.prodCplx((2, -1), (-1, 3)), (1, 7))
 
-    def test_conjugado(self):
-        self.assertEqual(
-            biblioteca.conjugadoClpx((2, -1)),
-            (2, 1)
-        )
+    def test_producto_2(self):
+        self.assertEqual(biblioteca.prodCplx((1, 1), (1, -1)), (2, 0))
 
-    def test_fase(self):
-        self.assertAlmostEqual(
-            biblioteca.faseClpx((1, 1)),
-            0.79,
-            places=2
-        )
+    def test_division_1(self):
+        self.assertEqual(biblioteca.divisionCplx((2, -1), (-1, 3)), (-0.5, -0.5))
+
+    def test_division_2(self):
+        self.assertEqual(biblioteca.divisionCplx((1, 1), (1, 1)), (1.0, 0.0))
+
+    def test_modulo_1(self):
+        self.assertEqual(biblioteca.moduloClpx((3, 4)), 5.0)
+
+    def test_modulo_2(self):
+        self.assertEqual(biblioteca.moduloClpx((0, 0)), 0.0)
+
+    def test_conjugado_1(self):
+        self.assertEqual(biblioteca.conjugadoClpx((2, -1)), (2, 1))
+
+    def test_conjugado_2(self):
+        self.assertEqual(biblioteca.conjugadoClpx((3, 4)), (3, -4))
+
+    def test_polar_a_cartesiano_1(self):
+        self.assertEqual(biblioteca.conversionPolarCartesianoClpx((1, 0)), (1.0, 0.0))
+
+    def test_polar_a_cartesiano_2(self):
+        self.assertEqual(biblioteca.conversionPolarCartesianoClpx((2, 0)), (2.0, 0.0))
+
+    def test_cartesiano_a_polar_1(self):
+        self.assertEqual(biblioteca.conversionCartesianoPolarClpx((3, 4)), (5.0, 0.93))
+
+    def test_cartesiano_a_polar_2(self):
+        self.assertEqual(biblioteca.conversionCartesianoPolarClpx((1, 0)), (1.0, 0.0))
+
+    def test_fase_1(self):
+        self.assertAlmostEqual(biblioteca.faseClpx((1, 1)), 0.79, places=2)
+
+    def test_fase_2(self):
+        self.assertAlmostEqual(biblioteca.faseClpx((0, 1)), 1.57, places=2)
 
 if __name__ == "__main__":
     unittest.main()
